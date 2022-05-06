3e17223e1f35fd874df3885ad9c53b3fc3a1f86b
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-05-06 19:12:25 -0400

    MOD: Testing with JWT

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,11 @@
+const jwt = require("jsonwebtoken");
 const supertest = require("supertest");
 const app = require("./../src/index");
 const request = supertest(app);
+require("dotenv").config();
+
+const noExpirationToken =
+  "Bearer " + jwt.sign("testing-purpose", process.env.TOKEN_SECRET);
 
 describe("API correct entry data test", () => {
   describe("GET /", () => {
@@ -22,15 +27,19 @@ describe("API correct entry data test", () => {
         username: "iam-user",
         schedules: [],
       });
+      authToken = res.body.token;
 
       expect(res.status).toBe(201);
       expect(res.body.statusOk).toBe(true);
+      expect(res.body.token == null).toBe(false);
     });
   });
 
   describe("GET /users", () => {
     it("Example get all users", async () => {
-      const res = await request.get("/users");
+      const res = await request
+        .get("/users")
+        .set("Authorization", noExpirationToken);
 
       expect(res.status).toBe(200);
       expect(res.body.statusOk).toBe(true);
@@ -41,7 +50,9 @@ describe("API correct entry data test", () => {
   describe("GET /user/:email", () => {
     it("Example get user by email", async () => {
       const exampleEmail = "aws@aws.lambda.com";
-      const res = await request.get(`/user/${exampleEmail}`);
+      const res = await request
+        .get(`/user/${exampleEmail}`)
+        .set("Authorization", noExpirationToken);
 
       expect(res.status).toBe(200);
       expect(res.body.statusOk).toBe(true);
@@ -65,10 +76,13 @@ describe("API correct entry data test", () => {
 
   describe("PUT /user/update/:email", () => {
     it("Example put user update", async () => {
-      const res = await request.put("/user/update/aws@aws.lambda.com").send({
-        phone: "90909090090",
-        username: "test-iam",
-      });
+      const res = await request
+        .put("/user/update/aws@aws.lambda.com")
+        .send({
+          phone: "90909090090",
+          username: "test-iam",
+        })
+        .set("Authorization", noExpirationToken);
 
       expect(res.status).toBe(200);
       expect(res.body.statusOk).toBe(true);
@@ -78,11 +92,14 @@ describe("API correct entry data test", () => {
   describe("POST /user/by/personal_info", () => {
     it("Example get user by personal info", async () => {
       const responseName = "test-iam";
-      const res = await request.post("/user/by/personal_info").send({
-        email: "",
-        username: "",
-        phone: "90909090090",
-      });
+      const res = await request
+        .post("/user/by/personal_info")
+        .send({
+          email: "",
+          username: "",
+          phone: "90909090090",
+        })
+        .set("Authorization", noExpirationToken);
 
       expect(res.status).toBe(200);
       expect(res.body.statusOk).toBe(true);
@@ -92,7 +109,9 @@ describe("API correct entry data test", () => {
 
   describe("DELETE /user/delete/:email", () => {
     it("Example deletee user remove", async () => {
-      const res = await request.delete("/user/delete/aws@aws.lambda.com");
+      const res = await request
+        .delete("/user/delete/aws@aws.lambda.com")
+        .set("Authorization", noExpirationToken);
 
       expect(res.status).toBe(200);
       expect(res.body.statusOk).toBe(true);
@@ -101,7 +120,9 @@ describe("API correct entry data test", () => {
 
   describe("GET /users", () => {
     it("Example get all users after delete", async () => {
-      const res = await request.get("/users");
+      const res = await request
+        .get("/users")
+        .set("Authorization", noExpirationToken);
 
       expect(res.status).toBe(200);
       expect(res.body.statusOk).toBe(true);
